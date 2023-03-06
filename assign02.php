0674e54c0977d7bdcea3510145678a5b74c473b0
Dawan Savage Bell <savagebell00@gmail.com>
2023-03-06 09:58:01 -0400

    Everything functional and working.Must add redirect to refresh form variables

--- assign02.php
+++ assign02.php
@@ -1,25 +1,31 @@
 <!--  form layout -->
+<?php
+    $house_num = $prop_length = $prop_width = $grass_type = $num_of_trees = $total_cost = "";
+?>
 
 <form action="assign02.php" method="post">
-    <p>Enter House Number: <input type="number" name="house_num" /></p>
+    <p>Enter House Number: <input type="number" min="0" name="house_num" /></p>
 
-    <p>Enter property length: <input type="number" name="prop_length" /></p>
+    <p>Enter property length (feet): <input type="number" min="0" name="prop_length" /></p>
 
-    <p>Enter property width: <input type="number" name="prop_width" /></p>
+    <p>Enter property width (feet): <input type="number" min="0" name="prop_width" /></p>
 
     <p>Choose type of grass: <input type="radio" name="grass_type"
     <?php if (isset($grass_type) && $grass_type =="fescue")?>
     value="fescue">fescue
+
     <input type="radio" name="grass_type"
     <?php if (isset($grass_type) && $grass_type =="bentgrass")?>
     value="bentgrass">bentgrass
+
     <input type="radio" name="grass_type"
     <?php if (isset($grass_type) && $grass_type =="campus")?>
     value="campus">campus
 
-    <p>Enter the number of trees: <input type="number" name="num_of_trees" /></p>
+    <p>Enter the number of trees: <input type="number" min="0" name="num_of_trees" /></p>
 
     <p><input type="submit" name="landscape_form" value="submit"/></p>
+
 </form>
 
 <!-- Retriving the data -->
@@ -31,30 +37,43 @@
         // validating houseNum
         if(empty($_POST['house_num']))
         {
-            $error_message .= "<li>You forgot to enter house number!</li>";
+            $error_message .= "<li>Error: You forgot to enter house number!</li>";
         } 
+        elseif($_POST['house_num'] <= 0)
+        {
+            $error_message .= "<li> Error: House Number must be greater than 0!";
+        }
         else
         {
-            $house_num = $_POST['house_num'];
+            $house_num = test_input($_POST['house_num']);
         }
 
         // validating propLength
         if(empty($_POST['prop_length']))
         {
-            $error_message .= "<li>You forgot to enter property length!</li>";
+            $error_message .= "<li>Error: You forgot to enter property length!</li>";
         } 
+        elseif($_POST['prop_length'] <= 0)
+        {
+            $error_message .= "<li> Error: Property Length must be greater than 0!";
+        }
         else 
         {
-            $prop_length = $_POST['prop_length'];
+            $prop_length = test_input($_POST['prop_length']);
         }
 
         // Validating propertyWidth
         if(empty($_POST['prop_width']))
         {
+            $error_message .="<li>Error: You forgot to enter Property Width!</li>";
+        }
+        elseif($_POST['prop_width'] <= 0)
+        {
+            $error_message .= "<li> Error: Property Width must be greater than 0";
         }
         else
         {
-            $prop_width = $_POST['prop_width'];
+            $prop_width = test_input($_POST['prop_width']);
         }
 
         // Validating grassType
@@ -72,9 +91,13 @@
             $error_message .= "<li>You must enter a number of trees!</li>";
 
         }
+        elseif($_POST['num_of_trees'] < 0)
+        {
+            $error_message .= "<li> Error: Number of trees must be 0 or greater!";
+        }
         else
         {
-            $num_of_trees = $_POST['num_of_trees'];
+            $num_of_trees = test_input($_POST['num_of_trees']);
 
         }
 
@@ -89,14 +112,61 @@
 <!-- Displaying inputs -->
 <?php
     echo "<h2> Input was:</h2>";
-    echo $house_num;
-    echo "<br>";
-    echo $prop_length;
-    echo "<br>";
-    echo $prop_width;
-    echo "<br>";
-    echo $grass_type;
-    echo "<br>";
-    echo $num_of_trees;
-    echo "<br>";
+    echo "Total cost for house " . $house_num . " is: " . calculate_price($prop_length,$prop_width,$grass_type, $num_of_trees);
+    // echo "<br>";
+    // echo $house_num;
+    // echo "<br>";
+    // echo $prop_length;
+    // echo "<br>";
+    // echo $prop_width;
+    // echo "<br>";
+    // echo $grass_type;
+    // echo "<br>";
+    // echo $num_of_trees;
+    // echo "<br>";
+?>
+
+<!-- Validation Function -->
+<?php
+    function test_input($data)
+    {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        
+        return $data;
+    }
+?>
+
+<!-- Calculate Price Function -->
+<?php
+    function calculate_price($prop_length, $prop_width,$grass_type, $num_of_trees)
+    {
+        $total_cost = 0;
+        $grass_cost= 0;
+        $base_labour = 1000;
+        $tree_price = $num_of_trees * 100;
+        $surface = $prop_length * $prop_width;
+        if($surface > 5000)
+        {
+            $surface += 500;
+        }
+
+        if($grass_type == "fescue")
+        {
+            $grass_cost = .05;
+        }
+        elseif($grass_type == "bentgrass")
+        {
+            $grass_cost = .02;
+        }
+        else
+        {
+            $grass_cost = .01;
+        }
+        
+        $total_cost =($surface * $grass_cost) + $tree_price + $base_labour;
+        return "$" . number_format((float)$total_cost, 2, '.','');
+
+    }
 ?>
--- index.php
+++ index.php
@@ -5,9 +5,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Assignment02</title>
-    <link rel="stylesheet" href="">
 </head>
 <body>
+
+    <?php
+        echo "<h2> Landscape Calculator Form!</h2>";
+    ?>
+
+    <!-- Form php Script -->
     <?php
         include("assign02.php");
     ?>
